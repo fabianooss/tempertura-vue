1ae75def8c206dbe91af332d54d0dc4e8b024791
FABIANO OSS <fabiano.oss@prof.sc.senac.br>
2022-09-19 19:42:56 -0300

    Conversor

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <conversor-temperatura 
+            :temperturaInicial="1"
+            :temperaturaFinal="100" >
+
+        </conversor-temperatura>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ConversorTemperatura from './components/ConversorTemperatura.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ConversorTemperatura
   }
+  
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  border: 1px solid silver;
+  padding: 20px 5px 5px 20px;
+
 }
 </style>
---
+++ src/components/ConversorTemperatura.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Temperatura</h1>
+    <table>
+      <tr>
+        <th>Celsius</th>
+        <th>Farenheight</th>
+      </tr>  
+      <tr v-for="t in temperatura" :key="t.celsius">
+        <td>{{t.celsius}}</td>
+        <td>{{t.farenheight}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    temperturaInicial: Number,
+    temperaturaFinal: Number
+  },
+  data() {
+    return {
+      temperatura: []
+    }
+  },
+  mounted() {
+    this.calcularTemperatura()
+  },
+  methods: {
+    calcularTemperatura() {
+       for (let i = this.temperturaInicial; i <= this.temperaturaFinal; i++)  {
+          this.temperatura.push({
+             celsius: i,
+             farenheight: (i * 9 / 5)  + 32
+          }) 
+       }
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
